99e6f100caf4e08636f1538e2f7c646646d001c1
Aaron Nowack <aaronbnowack@gmail.com>
2016-01-07 15:59:59 -0500

    Forgot to add untrack files.

---
+++ tests/utils.py
@@ -0,0 +1,14 @@
+from textwrap import dedent
+
+class mock_file(object):
+    def __init__(self, data):
+        self.data = dedent(data)
+        self.lines = self.data.splitlines(True)
+        self.curr_line = 0
+
+    def readline(self):
+        if self.curr_line < len(self.lines):
+            self.curr_line += 1
+            return self.lines[self.curr_line-1]
+        else:
+            return ''
